aa59810dabbdf8a70551afadd6df8d98afb50642
Vivek <viveksonybcd@gmail.com>
2020-10-06 19:06:22 +0530

    Updates

---
+++ precache-manifest.4c24f5d8f6c9abaeb184f893daf36835.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20148016501881cb4304881300ac320",
+    "url": "/temporary/index.html"
+  },
+  {
+    "revision": "c25abfdd6dcf896dbb89",
+    "url": "/temporary/static/css/main.436a93f5.chunk.css"
+  },
+  {
+    "revision": "924c74cee88887f5013a",
+    "url": "/temporary/static/js/2.17e161cf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/temporary/static/js/2.17e161cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25abfdd6dcf896dbb89",
+    "url": "/temporary/static/js/main.dbc904bf.chunk.js"
+  },
+  {
+    "revision": "a21ff2b8e62ab5492aaf",
+    "url": "/temporary/static/js/runtime-main.e9af60a0.js"
+  },
+  {
+    "revision": "98aa612b462c0d78d58405f6671a5648",
+    "url": "/temporary/static/media/logo.98aa612b.png"
+  }
+]);
